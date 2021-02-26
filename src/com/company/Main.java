84841af510286866c8b9007111a927777f34289d
Firiril <frestiazestil@gmail.com>
2021-02-26 17:18:29 +0100

    initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Animal dog = new Animal();
+        dog.spieces = "dog";
+        dog.name = "Haiku";
+        dog.weight = 8.0;
+        dog.feed();
+        Phone IPhone = new Phone();
+        IPhone.screenSize = 4.5;
+        IPhone.producer = "Apple";
+        IPhone.model = "6S";
+        IPhone.operationSystem = "ios";
+
+        // write your code here
+    }
+}
